5792ec69f09640c34ded68bdf7e435931c444d4a
Asive Nzwana <s220029881@mandela.ac.za>
2023-09-25 08:31:43 +0200

    Crud working and all and Add User works and stuff

--- HelpingHands/Controllers/AdminController.cs
+++ HelpingHands/Controllers/AdminController.cs
@@ -1,14 +1,17 @@
 ﻿using HelpingHands.Data;
+using HelpingHands.Models.ViewModels;
+using HelpingHands.Models.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace HelpingHands.Controllers
 {
-    [Authorize(Roles = "A")]
+    //[Authorize(Roles = "A")]
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext _context;
-     
+
         public AdminController(ApplicationDbContext context)
         {
 
@@ -22,16 +25,18 @@ namespace HelpingHands.Controllers
             var cities = _context.City.ToList();
             var chronicCons = _context.ChronicCondition.ToList();
 
-            
+
             ViewBag.CountCities = cities.Count();
             ViewBag.CountChronicCon = chronicCons.Count();
-            ViewBag.CountUsers =  users.Count(); 
+            ViewBag.CountUsers = users.Count();
+
+            ViewBag.Users = users.Where(u => u.Archived == false).Take(6);
             return View();
 
         }
         public IActionResult Users()
         {
-            var users=_context.Users.ToList();
+            var users = _context.Users.Where(u=>u.Archived==false).ToList();
 
             ViewBag.Users = users;
 
@@ -40,7 +45,175 @@ namespace HelpingHands.Controllers
 
         public IActionResult AddUser()
         {
+
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SubmitUser(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool checkEMail = IsEmailAlreadyInUse(model.Email);
+
+                if (checkEMail == false)
+                {
+                    var newUser = new UserModel
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        CellNo = model.CellNo,
+                        Password = model.Password,
+                        UserType = model.UserType,
+
+                    };
+                    _context.Users.Add(newUser);
+                    await _context.SaveChangesAsync();
+
+                    int userId = newUser.UserID;
+                    if (model.UserType == "N")
+                    {
+                        var newNurse = new Nurse
+                        {
+
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            CellNo = model.CellNo,
+                            Password = model.Password,
+                            userID = userId,
+                        };
+                        _context.Nurse.Add(newNurse);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = $"Error: Email {model.Email} Is already in use";
+                    return RedirectToAction("AddUser", "Admin");
+                }
+                return RedirectToAction("Users", "Admin");
+            }
+            return View(model);
+        }
+
+        public IActionResult EditUser([FromRoute] int Id)
+        {
+            var user = _context.Users.Where(u => u.UserID == Id).FirstOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var role = user.UserType.Trim();
+
+            var roleName = "";
+
+            if (role == "O")
+            {
+                roleName = "Office Manager";
+            }
+            else if (role == "P")
+            {
+                roleName = "Patient";
+            }
+            else if (role == "N")
+            {
+                roleName = "Nurse";
+            }
+            else if (role == "A")
+            {
+                roleName = "Admin";
+            }
+
+            ViewBag.RoleName = roleName;
+            var editUser = new EditUserViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                UserType = user.UserType,
+                UserID = user.UserID,
+                Email = user.Email,
+                CellNo = user.CellNo
+            };
+
+            return View(editUser);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateUser(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Users.Where(u => u.UserID == model.UserID).FirstOrDefault();
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+                user.CellNo = model.CellNo;
+                 
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+
+                var role = model.UserType.Trim();
+
+                if (role=="N")
+                {
+                    var nurse = _context.Nurse.Where(n => n.userID == model.UserID).FirstOrDefault();
+                    nurse.FirstName = model.FirstName;
+                    nurse.LastName = model.LastName;
+                    nurse.Email = model.Email;
+                    nurse.CellNo = model.CellNo;
+
+                    _context.Nurse.Update(nurse);
+                    await _context.SaveChangesAsync();
+                }
+                else if (role =="P")
+                {
+                    var patient = _context.Patient.Where(p => p.userID == model.UserID).FirstOrDefault();
+
+                    patient.FirstName = model.FirstName;
+                    patient.LastName = model.LastName;
+                    patient.Email = model.Email;
+                    patient.CellNo = model.CellNo;
+
+                    _context.Patient.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToAction("Users", "Admin");
+            }
+
+             return View(model);
+        }
+
+        [HttpPost("/Admin/DeleteUser/{userId}")]
+        public JsonResult DeleteUser(int userId)
+        {
+            var user = _context.Users.Where(u => u.UserID ==userId).FirstOrDefault();
+
+            user.Archived = true;
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
+            
+            return Json(true);
+        }
+        public bool IsEmailAlreadyInUse(string email)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.Email == email);
+            return existingUser != null;
+        }
+
+
+
+
     }
 }
--- HelpingHands/Models/ViewModels/EditUserViewModel.cs
+++ HelpingHands/Models/ViewModels/EditUserViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace HelpingHands.Models.ViewModels
 {
-    public class AddUserViewModel
+    public class EditUserViewModel
     {
+        public int UserID { get; set; }
+        [Display(Name = "User Type")]
+        public string UserType { get; set; }
         [Required]
         [Display(Name = "First Name")]
         [StringLength(120, ErrorMessage = "The {0} must be at least {2} and at a max {1} characters long.", MinimumLength = 2)]
@@ -17,24 +19,9 @@ namespace HelpingHands.Models.ViewModels
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$", ErrorMessage = "Invalid E-mail Address Format.")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
-         ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
-        [DataType(DataType.Password)]
-        public string? Password { get; set; }
-
-        [Required(ErrorMessage = "Confirm password is required.")]
-        [Compare("Password", ErrorMessage = "Confirm password doesn't match, Type again!")]
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm Password")]
-        public string? ConfirmPassword { get; set; }
         [Required]
         [Display(Name = "Cell Number")]
         [RegularExpression(@"^(\+27|0)[0-9]{9}$", ErrorMessage = "Invalid Cell Number Format.")]
         public string? CellNo { get; set; }
-        [Required]
-        [Display(Name = "User Type")]
-        public string? UserType { get; set; }
     }
 }
